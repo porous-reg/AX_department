7067afd69174889bf020a0f69e073c3c02babb87
jongjin <jjj200109@gmail.com>
2026-03-18 14:45:14 +0900

    Show English faculty names in English mode

--- components/People.tsx
+++ components/People.tsx
@@ -51,85 +51,85 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "김산하",
-        en: "김산하",
+        en: "Sanha Kim",
         img: "/images/faculty/adjunct/김산하.jpg",
         website: "https://amselab.kaist.ac.kr/",
       },
       {
         ko: "이승철",
-        en: "이승철",
+        en: "Seungchul Lee",
         img: "/images/faculty/adjunct/이승철.jpg",
         website: "https://iailab.kaist.ac.kr/",
       },
       {
         ko: "김현진",
-        en: "김현진",
+        en: "Hyunjin Kim",
         img: "/images/faculty/adjunct/김현진.jpg",
         website: "http://cvbml.kaist.ac.kr/",
       },
       {
         ko: "오일권",
-        en: "오일권",
+        en: "Ilkwon Oh",
         img: "/images/faculty/adjunct/오일권.jpg",
         website: "https://srim.kaist.ac.kr/",
       },
       {
         ko: "박인규",
-        en: "박인규",
+        en: "Inkyu Park",
         img: "/images/faculty/adjunct/박인규.jpg",
         website: "http://mint.kaist.ac.kr/",
       },
       {
         ko: "이정철",
-        en: "이정철",
+        en: "Jeongcheol Lee",
         img: "/images/faculty/adjunct/이정철.jpg",
         website: "http://mnil.kaist.ac.kr/",
       },
       {
         ko: "구승범",
-        en: "구승범",
+        en: "Seungbeom Koo",
         img: "/images/faculty/adjunct/구승범.jpg",
         website: "https://mskbiodyn.kaist.ac.kr/",
       },
       {
         ko: "경기욱",
-        en: "경기욱",
+        en: "Gi-Wook Kyung",
         img: "/images/faculty/adjunct/경기욱.jpg",
         website: "http://irobot.kaist.ac.kr/",
       },
       {
         ko: "김성수",
-        en: "김성수",
+        en: "Seongsu Kim",
         img: "/images/faculty/adjunct/김성수.jpeg",
         website: "http://mdam.kaist.ac.kr/",
       },
       {
         ko: "김영진",
-        en: "김영진",
+        en: "Youngjin Kim",
         img: "/images/faculty/adjunct/김영진.jpg",
         website: "http://upm2.kaist.ac.kr/",
       },
       {
         ko: "공경철",
-        en: "공경철",
+        en: "Kyoungchul Kong",
         img: "/images/faculty/adjunct/공경철.jpg",
         website: "http://robotics.kaist.ac.kr/",
       },
       {
         ko: "윤국진",
-        en: "윤국진",
+        en: "Kuk-Jin Yoon",
         img: "/images/faculty/adjunct/윤국진.jpg",
         website: "http://vi.kaist.ac.kr/",
       },
       {
         ko: "윤용진",
-        en: "윤용진",
+        en: "Yongjin Yoon",
         img: "/images/faculty/adjunct/윤용진.jpg",
         website: "https://jdl.kaist.ac.kr/",
       },
       {
         ko: "유홍기",
-        en: "유홍기",
+        en: "Hongki Yoo",
         img: "/images/faculty/adjunct/유홍기.avif",
         website: "http://boom.kaist.ac.kr/",
       },
@@ -141,19 +141,19 @@ const adjunctFacultyGroups: FacultyGroup[] = [
       },
       {
         ko: "이봉재",
-        en: "이봉재",
+        en: "Bongjae Lee",
         img: "/images/faculty/adjunct/이봉재.png",
         website: "https://trad.kaist.ac.kr/",
       },
       {
         ko: "남영석",
-        en: "남영석",
+        en: "Youngseok Nam",
         img: "/images/faculty/adjunct/남영석.avif",
         website: "https://mel.kaist.ac.kr/",
       },
       {
         ko: "윤희택",
-        en: "윤희택",
+        en: "Heetaik Yoon",
         img: "/images/faculty/adjunct/윤희택.jpg",
         website: "https://smslab.kaist.ac.kr/",
       },
@@ -164,49 +164,49 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "김현정",
-        en: "김현정",
+        en: "Hyunjeong Kim",
         img: "/images/faculty/adjunct/김현정.jpg",
         website: "https://msslab.kaist.ac.kr/",
       },
       {
         ko: "김희영",
-        en: "김희영",
+        en: "Heeyoung Kim",
         img: "/images/faculty/adjunct/김희영.png",
         website: "https://istat.kaist.ac.kr/",
       },
       {
         ko: "박진규",
-        en: "박진규",
+        en: "Jinkyu Park",
         img: "/images/faculty/adjunct/박진규.jpg",
         website: "https://ilp.kaist.ac.kr/sub0303/view/id/49",
       },
       {
         ko: "박찬영",
-        en: "박찬영",
+        en: "Chanyoung Park",
         img: "/images/faculty/adjunct/박찬영.jpg",
         website: "https://dsail.kaist.ac.kr/",
       },
       {
         ko: "송호승",
-        en: "송호승",
+        en: "Hoseung Song",
         img: "/images/faculty/adjunct/송호승.jpg",
         website: "https://hoseungs.github.io/",
       },
       {
         ko: "송환준",
-        en: "송환준",
+        en: "Hwanjun Song",
         img: "/images/faculty/adjunct/송환준.png",
         website: "https://disl-lab.github.io/",
       },
       {
         ko: "이종석",
-        en: "이종석",
+        en: "Jong-Seok Lee",
         img: "/images/faculty/adjunct/이종석.jpg",
         website: "http://scholar.google.com/citations?user=7BxzwPQAAAAJ&hl=en",
       },
       {
         ko: "홍기혁",
-        en: "홍기혁",
+        en: "Kihyuk Hong",
         img: "/images/faculty/adjunct/홍기혁.webp",
         website: "https://www.kihyukhong.com/",
       },
@@ -226,7 +226,7 @@ const adjunctFacultyGroups: FacultyGroup[] = [
       },
       {
         ko: "김재홍",
-        en: "김재홍",
+        en: "Jaehong Kim",
         img: "/images/faculty/adjunct/김재홍.jpg",
         website: "http://concrete.kaist.edu/",
       },
@@ -282,60 +282,60 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "이철호",
-        en: "이철호",
+        en: "Chul Ho Lee",
         img: "/images/faculty/adjunct/이철호.jpg",
         website: "http://sites.google.com/view/irontigerlee-kaist",
       },
       {
         ko: "김형철",
-        en: "김형철",
+        en: "Hyungchul Kim",
         img: "/images/faculty/adjunct/김형철.png",
       },
       {
         ko: "정승원",
-        en: "정승원",
+        en: "Seungwon (Eugene) Jeong",
         img: "/images/faculty/adjunct/정승원.jpg",
         website: "https://www.eugenejeong.com/",
       },
       {
         ko: "최현수",
-        en: "최현수",
+        en: "Hyunsoo Choi",
         img: "/images/faculty/adjunct/최현수.png",
         website: "http://sites.google.com/site/hschoi31/",
       },
       {
         ko: "신승훈",
-        en: "신승훈",
+        en: "Sean Seunghun Shin",
         img: "/images/faculty/adjunct/신승훈.png",
         website: "https://seanshin.com/",
       },
       {
         ko: "한진수",
-        en: "한진수",
+        en: "Jin Soo Han",
         img: "/images/faculty/adjunct/한진수.jpg",
         website: "http://sites.google.com/view/jinsoohan",
       },
       {
         ko: "김민기",
-        en: "김민기",
+        en: "Minki Kim",
         img: "/images/faculty/adjunct/김민기.jpg",
         website: "https://sites.google.com/site/minkikimquant/",
       },
       {
         ko: "김경국",
-        en: "김경국",
+        en: "Kyoung-Kuk Kim",
         img: "/images/faculty/adjunct/김경국.png",
         website: "https://sites.google.com/site/catenoid/",
       },
       {
         ko: "신동혁",
-        en: "신동혁",
+        en: "Donghyuk Shin",
         img: "/images/faculty/adjunct/신동혁.jpg",
         website: "http://dshin32.github.io/",
       },
       {
         ko: "신은철",
-        en: "신은철",
+        en: "Euncheol Shin",
         img: "/images/faculty/adjunct/신은철.png",
         website: "http://sites.google.com/site/euncheolshin00",
       },
@@ -349,31 +349,31 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "공승현",
-        en: "공승현",
+        en: "Seunghyun Kong",
         img: "/images/faculty/adjunct/공승현.jpg",
         website: "http://ave.kaist.ac.kr/",
       },
       {
         ko: "금동석",
-        en: "금동석",
+        en: "Dongseok Geum",
         img: "/images/faculty/adjunct/금동석.jpg",
         website: "http://axelab.kaist.ac.kr/",
       },
       {
         ko: "김인희",
-        en: "김인희",
+        en: "Inhi Kim",
         img: "/images/faculty/adjunct/김인희.jpg",
         website: "http://inhi.kim/",
       },
       {
         ko: "안승영",
-        en: "안승영",
+        en: "Seungyoung Ahn",
         img: "/images/faculty/adjunct/안승영.jpg",
         website: "http://emc.kaist.ac.kr/",
       },
       {
         ko: "이윤구",
-        en: "이윤구",
+        en: "Yungu Lee",
         img: "/images/faculty/adjunct/이윤구.jpg",
         website: "https://bsl.kaist.ac.kr/",
       },
@@ -387,19 +387,19 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "홍승범",
-        en: "홍승범",
+        en: "Seungbeom Hong",
         img: "/images/faculty/adjunct/홍승범.jpg",
         website: "http://mii.kaist.ac.kr/",
       },
       {
         ko: "서동화",
-        en: "서동화",
+        en: "Donghwa Seo",
         img: "/images/faculty/adjunct/서동화.jpg",
         website: "https://sites.google.com/view/seo-research-group",
       },
       {
         ko: "김동훈",
-        en: "김동훈",
+        en: "Donghoon Kim",
         img: "/images/faculty/adjunct/김동훈.jpg",
         website: "https://caim.kaist.ac.kr/",
       },
@@ -433,13 +433,13 @@ const adjunctFacultyGroups: FacultyGroup[] = [
       },
       {
         ko: "이상엽",
-        en: "이상엽",
+        en: "Sang Yup Lee",
         img: "/images/faculty/adjunct/이상엽.jpg",
         website: "http://mbel.kaist.ac.kr/",
       },
       {
         ko: "김지한",
-        en: "김지한",
+        en: "Jihan Kim",
         img: "/images/faculty/adjunct/김지한.jpg",
         website: "http://molsim.kaist.ac.kr/",
       },
@@ -453,12 +453,12 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "김동혁",
-        en: "김동혁",
+        en: "Donghyuk Kim",
         img: "/images/faculty/adjunct/익명.jpg",
       },
       {
         ko: "조병관",
-        en: "조병관",
+        en: "Byung-Kwan Cho",
         img: "/images/faculty/adjunct/조병관.png",
         website: "http://cholab.or.kr/",
       },
@@ -472,7 +472,7 @@ const adjunctFacultyGroups: FacultyGroup[] = [
     members: [
       {
         ko: "안재욱",
-        en: "안재욱",
+        en: "Jaewook Ahn",
         img: "/images/faculty/adjunct/안재욱.png",
         website: "http://qcl.kaist.ac.kr/",
       },
@@ -498,7 +498,7 @@ const People: React.FC<{ lang: Language; title: string }> = ({ lang }) => {
           <div className="absolute top-3 right-3 w-2 h-2 bg-[#004191] rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
         </div>
         <p className="font-bold text-slate-100 mb-1 min-h-[28px] text-lg">
-          {member.ko}
+          {lang === "ko" ? member.ko : member.en}
         </p>
         <p className="text-[10px] text-[#004191] font-black tracking-widest uppercase">
           {roleLabel}
